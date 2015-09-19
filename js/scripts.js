5f92d5def911c0a7a156bba58edd915a268b45ba
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-09-19 16:18:42 +0200

    adding translated baby

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,49 @@
-console.log('hello, beautiful');
+document.querySelector('.edit--darken button').addEventListener('click', function() {
+  var movingElem = document.querySelector('.edit--darken .images');
+  console.log('hi');
+  if (movingElem.hasAttribute('data-state')) {
+    movingElem.removeAttribute('data-state');
+  }
+  else {
+    movingElem.setAttribute('data-state', 'overlap');
+  }
+});
 
-document.querySelector('.hue-rotate button').addEventListener('click', function() {
-  var movingElem = document.querySelector('.hue-rotate .images');
+document.querySelector('.edit--lighten button').addEventListener('click', function() {
+  var movingElem = document.querySelector('.edit--lighten .images');
+  console.log('hi');
+  if (movingElem.hasAttribute('data-state')) {
+    movingElem.removeAttribute('data-state');
+  }
+  else {
+    movingElem.setAttribute('data-state', 'overlap');
+  }
+});
+
+document.querySelector('.edit--contrast button').addEventListener('click', function() {
+  var movingElem = document.querySelector('.edit--contrast .images');
+  console.log('hi');
+  if (movingElem.hasAttribute('data-state')) {
+    movingElem.removeAttribute('data-state');
+  }
+  else {
+    movingElem.setAttribute('data-state', 'overlap');
+  }
+});
+
+document.querySelector('.edit--composite button').addEventListener('click', function() {
+  var movingElem = document.querySelector('.edit--composite .images');
+  console.log('hi');
+  if (movingElem.hasAttribute('data-state')) {
+    movingElem.removeAttribute('data-state');
+  }
+  else {
+    movingElem.setAttribute('data-state', 'overlap');
+  }
+});
+
+document.querySelector('.edit--comparative button').addEventListener('click', function() {
+  var movingElem = document.querySelector('.edit--comparative .images');
   console.log('hi');
   if (movingElem.hasAttribute('data-state')) {
     movingElem.removeAttribute('data-state');
